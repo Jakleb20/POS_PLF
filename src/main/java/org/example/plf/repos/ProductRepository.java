10f2e6d6f3a0b7eda3fe9c0cf1b2bbedd3ec4992
leonj <jakleb20@htl-kaindorf.at>
2024-12-01 22:05:44 +0100

    added query in ProductRepository

--- src/main/java/org/example/plf/repos/ProductRepository.java
+++ src/main/java/org/example/plf/repos/ProductRepository.java
@@ -3,10 +3,14 @@ package org.example.plf.repos;
 import org.example.plf.models.Location;
 import org.example.plf.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     public int countProductByLocation(Location location);
     public Product getProductById(int id);
+    @Query("SELECT COUNT(p) FROM Product p WHERE p.location.name = :name")
+    Integer countProductsByLocationName(@Param("name") String name);
 }
